cc5c8d79d66d2ccbc0e9b33797c582eac3e090d0
Abdulaziz Alsenafi <asenafi@kisr.edu.kw>
2019-01-06 13:45:18 +0300

    task19+20 solo sub

--- api/serializers.py
+++ api/serializers.py
@@ -1,17 +1,68 @@
 from rest_framework import serializers
-from restaurants.models import Restaurant
+from restaurants.models import Restaurant, Item
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'first_name',
+            'last_name',
+            'email'
+        ]
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ['name', 'description', 'price']
+
 
 class RestaurantListSerializer(serializers.ModelSerializer):
+    detail = serializers.HyperlinkedIdentityField(
+        view_name="api-detail",
+        lookup_field="id",
+        lookup_url_kwarg="restaurant_id"
+    )
+    update = serializers.HyperlinkedIdentityField(
+        view_name="api-update",
+        lookup_field="id",
+        lookup_url_kwarg="restaurant_id"
+    )
+    delete = serializers.HyperlinkedIdentityField(
+        view_name="api-delete",
+        lookup_field="id",
+        lookup_url_kwarg="restaurant_id"
+    )
+
     class Meta:
         model = Restaurant
         fields = [
             'name',
             'opening_time',
             'closing_time',
-            ]
+            'detail',
+            'update',
+            'delete'
+        ]
 
 
 class RestaurantDetailSerializer(serializers.ModelSerializer):
+    update = serializers.HyperlinkedIdentityField(
+        view_name="api-update",
+        lookup_field="id",
+        lookup_url_kwarg="restaurant_id"
+    )
+    delete = serializers.HyperlinkedIdentityField(
+        view_name="api-delete",
+        lookup_field="id",
+        lookup_url_kwarg="restaurant_id"
+    )
+    owner = UserSerializer()
+    items = serializers.SerializerMethodField()
+
     class Meta:
         model = Restaurant
         fields = [
@@ -21,7 +72,16 @@ class RestaurantDetailSerializer(serializers.ModelSerializer):
             'description',
             'opening_time',
             'closing_time',
-            ]
+            'update',
+            'delete',
+            'items'
+        ]
+
+    def get_items(self, obj):
+        itemlist = Item.objects.filter(restaurant=obj)
+        items = ItemSerializer(itemlist, many=True).data
+        return items
+
 
 class RestaurantCreateUpdateSerializer(serializers.ModelSerializer):
     class Meta:
@@ -31,4 +91,4 @@ class RestaurantCreateUpdateSerializer(serializers.ModelSerializer):
             'description',
             'opening_time',
             'closing_time',
-            ]
+        ]
